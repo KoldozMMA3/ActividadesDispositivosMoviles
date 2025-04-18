967cb76c3910f45f427981bdeda444d94e060de5
Cristian Luna <128846559+KoldozMMA3@users.noreply.github.com>
2025-04-17 19:26:28 -0500

    Desarrolle la implemenatcion de la clase udusrio

--- app/src/main/java/com/example/desarrollo_caso1_practica4/Usuario.kt
+++ app/src/main/java/com/example/desarrollo_caso1_practica4/Usuario.kt
@@ -1,2 +1,12 @@
 package com.example.desarrollo_caso1_practica4
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Usuario(
+    val nombre: String,
+    val edad: String,
+    val ciudad: String,
+    val correo: String
+) : Parcelable
